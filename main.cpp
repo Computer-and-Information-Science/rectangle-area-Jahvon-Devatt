ff6ee22a116ba8f57a61439060249fb1d6cc3022
Jahvon-Devatt <jahvon.devatt@student.northampton.edu>
2026-01-29 17:54:47 +0000

    Created Rectangle Area/Perimeter program

--- main.cpp
+++ main.cpp
@@ -2,8 +2,18 @@
 using namespace std;
 
 int main() {
+
   const double length = 7.2;
   const double width = 1.3;
 
+  cout << "Rectangle Properties: \n";
+  cout << "Length = " << length << endl;
+  cout << "Width = " << width << endl;
+
+  double area = length * width;
+  double perimeter = (2*length)+(2*width);
+
+  cout << "Area = " << area << endl;
+  cout << "Perimeter = " << perimeter << endl;
 
 }
